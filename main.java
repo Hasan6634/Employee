21415a6aa60e0350c6bb88a5f99504acb549daa7
Hasan6634 <138626695+Hasan6634@users.noreply.github.com>
2023-10-04 16:24:38 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+package Employee;
+
+public class main {
+    public static void main(String[] args) {
+        Employee Hasan=new Employee("Hasan",2000,45,1985);
+        Hasan.string();
+
+    }
+}
